ed57cdab9a3390df9d0de1a608a21dd34581735e
Bryan Liu <liub3@bxscience.edu>
2023-09-27 11:31:58 -0400

    lift PID implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Lift.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Hardware.Mechanism;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.control.PIDFController;
@@ -26,20 +30,27 @@ public class Lift implements Mechanism {
     public static final double shoulderkP = 0;
     public static final double shoulderkI = 0;
     public static final double shoulderkD = 0;
+    public static int MAX_VEL_SHOULDER = 60;
+    public static int MAX_ACCEL_SHOULDER = 60;
 
 
     public static final double elbowkP = 0;
     public static final double elbowkI = 0;
     public static final double elbowkD = 0;
+    public static int MAX_VEL_ELBOW = 60;
+    public static int MAX_ACCEL_ELBOW = 60;
 
     PIDCoefficients shoulderPID = new PIDCoefficients(shoulderkP, shoulderkI, shoulderkD);
     // create the controller
     PIDFController shoulderController = new PIDFController(shoulderPID, 0, 0 ,0, (p, v) -> getShoulderFg(p, v));
+    public static MotionProfile profileShoulder;
+    ElapsedTime timeShoulder = new ElapsedTime();
 
     PIDCoefficients elbowPID = new PIDCoefficients(shoulderkP, shoulderkI, shoulderkD);
     // create the controller
     PIDFController elbowController = new PIDFController(elbowPID, 0, 0, 0, (p, v) -> getElbowFg(p, v));
-
+    public static MotionProfile profileElbow;
+    ElapsedTime timeElbow = new ElapsedTime();
 
     boolean isReached = false;
 
@@ -51,17 +62,66 @@ public class Lift implements Mechanism {
         //DO NOT RESET THE ENCODERS, WE WANT TO MAINTAIN POSITION
         shoulder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         elbow.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
+    public void shoulderGoToAngle(double angle) {
+        double prevPos = shoulder.getCurrentPosition();
+        if (angleToCountShoulder(angle) == prevPos) return;
+        double target = angleToCountShoulder(angle);
+
+        profileShoulder = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(prevPos, 0, 0),
+                new MotionState(target, 0, 0),
+                MAX_VEL_SHOULDER,
+                MAX_ACCEL_SHOULDER
+        );
+        timeShoulder.reset();
+    }
+
+    public void elbowGoToAngle(double angle) {
+        double prevPos = elbow.getCurrentPosition();
+        if (angleToCountElbow(angle) == prevPos) return;
+        double target = angleToCountShoulder(angle);
+
+        profileShoulder = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(prevPos, 0, 0),
+                new MotionState(target, 0, 0),
+                MAX_VEL_ELBOW,
+                MAX_ACCEL_ELBOW
+        );
+        timeElbow.reset();
+    }
+
+    public void PIDUpdateShoulder() {
+        MotionState state = profileShoulder.get(timeShoulder.seconds());
+        shoulderController.setTargetPosition(state.getX());
+        shoulderController.setTargetVelocity(state.getV());
+        shoulderController.setTargetAcceleration(state.getA());
+        double power = shoulderController.update(shoulder.getCurrentPosition());
+        shoulder.setPower(power);
+    }
+
+    public void PIDUpdateElbow() {
+        MotionState state = profileElbow.get(timeElbow.seconds());
+        elbowController.setTargetPosition(state.getX());
+        elbowController.setTargetVelocity(state.getV());
+        elbowController.setTargetAcceleration(state.getA());
+        double power = elbowController.update(elbow.getCurrentPosition());
+        elbow.setPower(power);
+    }
+
+
     public double angleToCountShoulder(double angle) {
-        double counts = angle*360/CPR;
+        double counts = (360/angle)/CPR;
         //TODO: may be negative sign
         return shoulder0 + counts;
 
     }
 
     public double angleToCountElbow(double angle) {
-        double counts = angle*360/CPR;
+        double counts = (360/angle)/CPR;
         //TODO: may be negative sign
         return elbow180 + counts;
     }
