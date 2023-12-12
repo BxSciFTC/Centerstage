89f9df0e38ce4c1f98dc145437285e27635c2d3a
BryanNoBryan <121914672+BryanNoBryan@users.noreply.github.com>
2023-12-12 16:43:22 -0500

    new arm2's pid and reset position done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/testing/armTesting/NewArm2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/testing/armTesting/NewArm2.java
@@ -6,8 +6,10 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 import org.firstinspires.ftc.teamcode.hardware.Mechanism;
 
 @Config
@@ -16,26 +18,40 @@ public class NewArm2 extends LinearOpMode implements Mechanism {
     HardwareMap hwMap;
     DcMotorEx shoulder;
     DcMotorEx elbow;
+    TouchSensor shoulderTouch;
+    boolean prevShoulderTouch;
+    TouchSensor elbowTouch;
+    boolean prevElbowTouch;
 
     private PIDController shoulderController;
-    public static double p1 = 0.1, i1 = 0, d1 = 0;
+    public static double p1 = 0.03, i1 = 0, d1 = 0;
 
     private PIDController elbowController;
-    public static double p2 = 0.1, i2 = 0, d2 = 0;
+    public static double p2 = 0.03, i2 = 0, d2 = 0;
 
     public static int shoulderTarget = 0;
     public static int elbowTarget = 0;
     public int shoulderPos = 0;
     public int elbowPos = 0;
 
+    public static double power1 = 0;
+    public static double power2 = 0;
+
+
     @Override
     public void init(HardwareMap hwMap) {
         this.hwMap = hwMap;
         shoulderController = new PIDController(p1, i1, d1);
-        elbowController = new PIDController(p1, i1, d1);
+        elbowController = new PIDController(p2, i2, d2);
 
         shoulder = hwMap.get(DcMotorEx.class, "shoulder");
         elbow = hwMap.get(DcMotorEx.class, "elbow");
+
+        shoulderTouch = hwMap.get(TouchSensor.class, "ShoulderTouch");
+        elbowTouch = hwMap.get(TouchSensor.class, "ElbowTouch");
+
+        prevShoulderTouch = shoulderTouch.isPressed();
+        prevElbowTouch = elbowTouch.isPressed();
     }
 
     public void PIDUpdate() {
@@ -43,15 +59,27 @@ public class NewArm2 extends LinearOpMode implements Mechanism {
         elbowController.setPID(p2, i2, d2);
 
 
+
         shoulderPos = shoulder.getCurrentPosition();
         double pid1  = shoulderController.calculate(shoulderPos, shoulderTarget);
-        double power1 = pid1 + shoulderFF();
+        power1 = pid1 + shoulderFF();
         shoulder.setPower(power1);
 
         elbowPos = elbow.getCurrentPosition();
         double pid2  = elbowController.calculate(elbowPos, elbowTarget);
-        double power2 = pid2 + elbowFF();
+        power2 = pid2 + elbowFF();
         elbow.setPower(power2);
+
+        if (shoulderTouch.isPressed()) {
+            calibrateShoulder();
+        } else {
+            prevShoulderTouch = false;
+        }
+        if (elbowTouch.isPressed()) {
+            calibrateElbow();
+        } else {
+            prevElbowTouch = false;
+        }
     }
 
     public double shoulderFF() {
@@ -60,6 +88,24 @@ public class NewArm2 extends LinearOpMode implements Mechanism {
 
     public double elbowFF() {
         return 0;
+    }
+
+    public void calibrateShoulder() {
+        if (prevShoulderTouch == false) {
+            shoulder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            shoulder.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            prevShoulderTouch = true;
+        } else
+            return;
+    }
+
+    public void calibrateElbow() {
+        if (prevElbowTouch == false) {
+            elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            elbow.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            prevElbowTouch = true;
+        } else
+            return;
     }
 
     @Override
@@ -74,6 +120,8 @@ public class NewArm2 extends LinearOpMode implements Mechanism {
             telemetry.addData("shoulderTarget", shoulderTarget);
             telemetry.addData("elbowPos", elbowPos);
             telemetry.addData("elbowTarget", elbowTarget);
+            telemetry.addData("power1", power1);
+            telemetry.addData("power2", power2);
             telemetry.update();
         }
     }
