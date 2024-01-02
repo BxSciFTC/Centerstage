f5ab6e08491235bc392da8a07de162e5d3d2bb50
Navil Talukdar <naviltalukdar3@gmail.com>
2024-01-02 16:41:27 -0500

    update auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auton/Constants.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opMode.auton;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+public class Constants {
+    // Starting positions
+    public static double START_X = 0;
+    public static double START_Y = 0;
+    public static double START_HEADING = Math.toRadians(0);
+
+    public static Pose2d BACKSTAGE_BLUE_START = new Pose2d(START_X, START_Y, START_HEADING);
+
+    public static double CENTER_SPIKE_MARK_X = 20;
+    public static double CENTER_SPIKE_MARK_Y = 20;
+
+    public static Vector2d LEFT_SPIKE_MARK = new Vector2d(CENTER_SPIKE_MARK_X + 6, CENTER_SPIKE_MARK_Y - 6);
+    public static Pose2d CENTER_SPIKE_MARK = new Pose2d(CENTER_SPIKE_MARK_X, CENTER_SPIKE_MARK_Y);
+    public static Vector2d RIGHT_SPIKE_MARK = new Vector2d(CENTER_SPIKE_MARK_X + 6, CENTER_SPIKE_MARK_Y + 6);
+
+}
