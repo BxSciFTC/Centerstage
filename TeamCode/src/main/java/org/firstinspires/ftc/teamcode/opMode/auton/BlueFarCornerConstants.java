af250de6fcbfcb26cee1f8ca105151382a4bf87e
CornFlakes <caih@bxscience.edu>
2024-01-08 16:57:13 -0500

    Reordered some constants and added another waypoint for better pathfinding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auton/BlueFarCornerConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auton/BlueFarCornerConstants.java
@@ -30,12 +30,18 @@ public class BlueFarCornerConstants {
     //public static final Vector2d placelocation1 = new Vector2d(place1locx, place1locy);
     //public static final Vector2d placelocation2 = new Vector2d(place2locx, place2locy);
     //public static final Vector2d placelocation3 = new Vector2d(place3locx, place3locy);
-    public static final Pose2d Robot2Beacon= new Pose2d();
-    public static double parkplacex=12;
-    public static double parkplacey=132;
-    public static final Pose2d parkplace = new Pose2d(parkplacex,parkplacey, Math.toRadians(270));
+    //implement camera to see beacons into beaconloc, changing the value to
+    // determine place location which will determine angle for this pose2d
+    public static final Pose2d Robot2Beacon= new Pose2d(beaconAreaPlacex, beaconAreaPlacey, Math.toRadians(-1));
     public static double boardx=36;
     public static double boardy=120;
     public static final Pose2d board = new Pose2d(boardx,boardy, Math.toRadians(90));
+    public static double preparkplacex=12;
+    public static double preparkplacey=120;
+    public static final Pose2d preparkplace = new Pose2d(preparkplacex,preparkplacey, Math.toRadians(270));
+    public static double parkplacex=12;
+    public static double parkplacey=132;
+    public static final Pose2d parkplace = new Pose2d(parkplacex,parkplacey, Math.toRadians(270));
+
 
 }
