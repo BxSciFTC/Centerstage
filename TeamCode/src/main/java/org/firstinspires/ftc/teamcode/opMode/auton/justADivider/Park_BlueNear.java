f528ee786ba3a28118a30e3e74673d1ae3234fee
HarrisonCai <caih@bxscience.edu>
2024-01-13 12:56:29 -0500

    Auto park

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auton/justADivider/Park_BlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auton/justADivider/Park_BlueNear.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.opMode.auton.justADivider;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.opMode.auton.BlueFarTrapConstants;
@@ -16,18 +18,23 @@ public class Park_BlueNear extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         drive = new SampleMecanumDrive(hardwareMap);
+        DcMotor leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        DcMotor leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        DcMotor rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        DcMotor rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         //deposit.init(hardwareMap);
 
-        TrajectorySequence move = drive.trajectorySequenceBuilder(BlueNearTrapConstants.start)
-                .strafeLeft(48)
-                .build();
 
         waitForStart();
         wait(10000);
-        drive.followTrajectorySequenceAsync(move);
+
 
         while (opModeIsActive() && !isStopRequested()) {
             drive.update();
+            leftFront.setPower(-0.5);
+            leftRear.setPower(0.5);
+            rightFront.setPower((0.5));
+            rightRear.setPower((-0.5));
         }
     }
 }
